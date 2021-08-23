17328863113fcd69943570dcb10558ee884c6d75
Neil Lamas <nlamas@icmab-csic.icmab.es>
2021-08-23 17:24:20 +0200

    Changed defaults and updated provider thicknesses

--- model_and_view/defaults.py
+++ model_and_view/defaults.py
@@ -13,12 +13,12 @@ GV = 83.1 # factor de conversio: Gauss per Volt -- 90 GV previously
 amplecinta = 12e-3 # ample aproximat de la cinta, pot tenir marge d'error de l'ordre del 10-20%
 # dx = 5e-4 # pas en X (entre files) de la sonda
 # dy = 2e-4 # pas en Y (dins de cada fila) de la sonda
-delta_x = 51 # pas en X (entre files) de la sonda
-delta_y = 26 # pas en Y (dins de cada fila) de la sonda
+delta_x = 62 # pas en X (entre files) de la sonda
+delta_y = 34 # pas en Y (dins de cada fila) de la sonda
 
 
 pas = 20
 
 ht = 0.3e-3 # alc,ada de mesura
 
-sample_thickness = 3e-6 #Theva and SuperOx 3e-6 , superpower:  1.35e-6
+sample_thickness = 2.5e-6 #SuperOx 3e-6 , superpower:  1.35e-6, Theva 2.8e-6, Fujikura (FESC) 2.5e-6
