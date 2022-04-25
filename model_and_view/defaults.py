5049a51da86fe3a5279fede966adff97d41e3567
Neil Lamas <nlamas@icmab-csic.icmab.es>
2022-04-25 11:50:03 +0200

    commit from ICMAB to merge

--- model_and_view/defaults.py
+++ model_and_view/defaults.py
@@ -1,17 +1,17 @@
 import os 
 
 
-sample_name ='Theva_normal_5cm_2_2021_12_1_16_53'
+sample_name ='SOx_25mm_bent_9_7_2022_4_21_13_25'
 
 input_path = os.path.join(r'G:\My Drive\PhD\EXPERIMENTS\Hall_routine\inputs')
 output_path = os.path.join(r'G:\My Drive\PhD\EXPERIMENTS\Hall_routine\outputs', sample_name)
 
 
-GV = 80.9 # factor de conversio: Gauss per Volt -- 90 GV previously, 83.1 or 80.9
+GV = 76.5 # factor de conversio: Gauss per Volt -- 90 GV previously, 83.1 or 80.9
 amplecinta = 12e-3 # ample aproximat de la cinta, pot tenir marge d'error de l'ordre del 10-20%
 # dx = 5e-4 # pas en X (entre files) de la sonda
 # dy = 2e-4 # pas en Y (dins de cada fila) de la sonda
-delta_x = 74 # pas en X (entre files) de la sonda
+delta_x = 85 # pas en X (entre files) de la sonda
 delta_y = 26 # pas en Y (dins de cada fila) de la sonda
 
 
@@ -19,4 +19,4 @@ pas = 20
 
 ht = 0.3e-3 # alc,ada de mesura
 
-sample_thickness = 2.8e-6 #SuperOx 3e-6 , superpower:  1.35e-6, Theva 2.8e-6, Fujikura (FESC) 2.5e-6
+sample_thickness = 3e-6 #SuperOx 3e-6 , superpower:  1.35e-6, Theva 2.8e-6, Fujikura (FESC) 2.5e-6
