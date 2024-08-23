3e943e445c10bc61665f4ad6ee7febabea93566b
Lamas Neil <neillamas@gmail.com>
2024-08-23 18:40:17 +0200

    improve comments of defaults

--- README.md
+++ README.md
@@ -11,19 +11,32 @@ Requires:
 Will be updated to Matlab2024 - Python3.7
 
 How to use:
+
 1) Download code:
    - Make sure you have downloaded all the software needed in the right versions (I recommend Matlab2021 or later - Python3.7 or later - VSCode - Git Bash)
    - Open a Git Bash terminal
    - Within the Git Bash terminal, move into the directory in which you want to download the software (e.g. "cd C:\Users\XXX\Desktop\workspace")
    - Enter following command: "git clone https://github.com/lamasne/hall_routine.git"
+
 2) Activate matlab use from python:
    - Open a command prompt as admin and enter:
-     "cd C:_matlabroot_\extern\engines\python" (where _matlabroot_ is the path to matlab on your machine, e.g. "C:\Program Files\MATLAB\R2022a")
-     "_pythonroot_ setup.py install" (where _pythonroot_ is the path to python on your machine, e.g. "C:\Users\Lamas\AppData\Local\Programs\Python\Python37\python.exe")
+     
+     "cd C:_matlabroot_\extern\engines\python" 
+     
+     (where _matlabroot_ is the path to matlab on your machine, e.g. "C:\Program Files\MATLAB\R2022a\extern\engines\python")
+
+   - Then, enter: 
+
+     "_pythonroot_ setup.py install" 
+     
+     (where _pythonroot_ is the path to python on your machine, e.g. "C:\Users\Lamas\AppData\Local\Programs\Python\Python37\python.exe setup.py install")
+
 3) Run code:
    - Open a code editor (e.g. Visual Studio code - might need to download it first)
    - Within your code editor, open the folder hall_routine
    - Press "run" (play button)
+
+4) Once it runs, you might want to change the values of input and output paths in model_and_view/defaults.py, so that the interface always opens with your hall probe folder suggested as input/output path
 
 Debugging:
 1) Check that you selected the right version of Python in your code editor
--- model_and_view/defaults.py
+++ model_and_view/defaults.py
@@ -1,24 +1,35 @@
-import os 
+import os
 
-sample_name ='FF_PSFC_free-standing_delam_25mm_1_2023_11_13_16_28'
+# Name of the input file
+sample_name = "FF_PSFC_free-standing_delam_25mm_1_2023_11_13_16_28"
 
-# input_path = os.path.join(r'H:\My Drive\PhD\EXPERIMENTS\Hall_routine\inputs\2nd_RADES_cavity_prepa\using small magnet')
-input_path = os.path.join(r'G:\My Drive\PhD\EXPERIMENTS\Hall_routine\inputs\2023')
-# output_path = os.path.join(r'H:\My Drive\PhD\EXPERIMENTS\Hall_routine\outputs\2022-04-05 - Half pipes', sample_name)
-output_path = os.path.join(r'G:\My Drive\PhD\EXPERIMENTS\Hall_routine\outputs', sample_name)
+# Path of the input file
+input_path = os.path.join(r"H:\My Drive\PhD\EXPERIMENTS\Hall_routine\inputs\2023")
 
+# Path of the output file
+output_path = os.path.join(
+    r"H:\My Drive\PhD\EXPERIMENTS\Hall_routine\outputs", sample_name
+)
+
+# Gauss per Volt conversion factor:  previously: 76.5, 90, 83.1, 80.9, 76.5
 GV = 513.6
-# GV = 76.5 # factor de conversio: Gauss per Volt -- 90 GV previously, 83.1, 80.9, 76.5 (from 22-04-22 - 81.7 calculated)
-amplecinta = 12e-3 # ample aproximat de la cinta, pot tenir marge d'error de l'ordre del 10-20%
-# dx = 5e-4 # pas en X (entre files) de la sonda
-# dy = 2e-4 # pas en Y (dins de cada fila) de la sonda
-delta_x = 41 # 35 - (Y in hall scan)
-delta_y = 26 # 26 - (X in hall scan)
 
+# Width of the tape (in m) - typically 12e-3
+amplecinta = 12e-3
+# Length of the scan (in mm) - (Y in hall scan)
+delta_x = 41
+# Width of the scan (in mm) - (X in hall scan) - typically 26 = 12 + 7*2
+delta_y = 26
+
+# Select only 1 out of "pas" values of scanning - Leave at 20 unless you know what you're doing
 pas = 20
 
-ht = 0.316e-3 #0.388 - distance probe to SC layer: Al foil = 16um ; sensor internal wall = 300um; Cu + Ag = 22um
+# Distance probe to SC layer: sensor epoxy wall = 300um; Al foil = 16um ; Cu + Ag = 22um
+ht = 316e-6
+# ht = 388e-6
 
-sample_thickness = 2.5e-6 #SuperOx 3e-61 , superpower:  1.35e-6, Theva 2.8e-6, Fujikura (FESC) 2.5e-6
+# Sample thickness: SuperOx 3e-6, superpower:  1.35e-6, Theva 2.8e-6, Fujikura (FESC) 2.5e-6
+sample_thickness = 2.5e-6
 
-filter_bool = 1 #1 #0
+# Activate savgol filtering of the input signal?
+filter_bool = 1  # 1 #0
